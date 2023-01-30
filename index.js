25ce86f00350afa8861fcdb145e4bf3654df4eee
Steak Barbare <cortodufour@gmail.com>
2023-01-30 04:32:17 +0100

    Refacto: Backspace & Comments

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const charactersRouter = require("./routes/characters");
+
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true
   })
 );
-app.get("/", (req, res) => {
+
+// Shows a "hello world" on the default link
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+// Shows an "ok" message on the default database link
+app.get("/db", (req, res) => {
   res.json({ message: "ok" });
 });
+
+// Used to visualize the "characters" table
 app.use("/characters", charactersRouter);
+
 /* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -19,15 +30,13 @@ app.use((err, req, res, next) => {
   res.status(statusCode).json({ message: err.message });
   return;
 });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
+// Let the "public" folder be seen by everyone, this is where the game logic should be placed
 app.use(express.static("public"));
-
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
 
 var server = app.listen(8081, function () {
   var host = server.address().address;
